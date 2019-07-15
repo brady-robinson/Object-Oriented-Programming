8bde85ddd03cd4bcce3f55a92bdbd246ef68d851
Brady Robinson <bradyrobinson3@gmail.com>
2019-07-15 18:33:04 -0400

    Add files to lesson_4

---
+++ lesson_4/medium_1/4.rb
@@ -0,0 +1,27 @@
+class Computer
+  attr_accessor :template
+
+  def create_template
+    @template = "template 14231"
+  end
+
+  def show_template
+    template
+  end
+end
+
+# class Computer
+#   attr_accessor :template
+
+#   def create_template
+#     self.template = "template 14231"
+#   end
+
+#   def show_template
+#     self.template
+#   end
+# end
+
+comp_temp = Computer.new
+comp_temp.create_template
+puts comp_temp.show_template
